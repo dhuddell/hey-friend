97dd3b86d85b2d7f2be3ef268431002b4e3f72fc
dhuddell <dan.huddell@gmail.com>
2019-01-25 10:32:49 -0500

    dependencies updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,8 +17,12 @@ module.exports = {
   },
   parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: '2018',
-    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      ecmaVersion: '2018',
+      sourceType: 'module',
+    }
   },
   plugins: [
     'import',

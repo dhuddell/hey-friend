f56f5f456a886a5bdd02acd0148365958673b633
dhuddell <dan.huddell@gmail.com>
2019-04-23 23:43:39 -0400

    added this data and query

---
+++ src/graphql/mocks/mock-friends-data.js
@@ -0,0 +1,166 @@
+export default {
+  friends:
+  [
+    {
+      name: 'Jim',
+      icon: 'fa-bolt',
+      friendId: '1',
+      description: 'best bro',
+      friendScore: 0,
+      goalSetCollection: {
+        targetGoal: {
+          phone: 1,
+          text: 5,
+          beer: 2,
+        },
+        currentGoal: {
+          phone: 0,
+          text: 2,
+          beer: 0,
+        },
+        cadence: 'monthly',
+      },
+    },
+    {
+      name: 'Jane',
+      icon: 'fa-book',
+      friendId: '2',
+      description: 'basic beach',
+      friendScore: 25,
+      goalSetCollection: {
+        targetGoal: {
+          phone: 1,
+          text: 5,
+          beer: 2,
+        },
+        currentGoal: {
+          phone: 0,
+          text: 2,
+          beer: 0,
+        },
+        cadence: 'monthly',
+      },
+    },
+    {
+      name: 'Joe',
+      icon: 'fa-handshake-o',
+      friendId: '3',
+      description: 'wingman ofc',
+      friendScore: 50,
+      goalSetCollection: {
+        targetGoal: {
+          phone: 1,
+          text: 5,
+          beer: 2,
+        },
+        currentGoal: {
+          phone: 0,
+          text: 2,
+          beer: 0,
+        },
+        cadence: 'monthly',
+      },
+    },
+    {
+      name: 'Jiminy',
+      icon: 'fa-bath',
+      friendId: '4',
+      description: 'a wise old cricket',
+      friendScore: 75,
+      goalSetCollection: {
+        targetGoal: {
+          phone: 1,
+          text: 5,
+          beer: 2,
+        },
+        currentGoal: {
+          phone: 0,
+          text: 2,
+          beer: 0,
+        },
+        cadence: 'monthly',
+      },
+    },
+    {
+      name: 'Keanu',
+      icon: 'fa-shower',
+      friendId: '5',
+      description: 'an invader',
+      friendScore: 50,
+      goalSetCollection: {
+        targetGoal: {
+          phone: 1,
+          text: 5,
+          beer: 2,
+        },
+        currentGoal: {
+          phone: 0,
+          text: 2,
+          beer: 0,
+        },
+        cadence: 'monthly',
+      },
+    },
+    {
+      name: 'Joany',
+      icon: 'fa-anchor',
+      friendId: '6',
+      description: 'who knows',
+      friendScore: 100,
+      goalSetCollection: {
+        targetGoal: {
+          phone: 1,
+          text: 5,
+          beer: 2,
+        },
+        currentGoal: {
+          phone: 0,
+          text: 2,
+          beer: 0,
+        },
+        cadence: 'monthly',
+      },
+    },
+    {
+      name: 'James',
+      icon: 'fa-500px',
+      friendId: '7',
+      description: 'goes to shows',
+      friendScore: 75,
+      goalSetCollection: {
+        targetGoal: {
+          phone: 1,
+          text: 5,
+          beer: 2,
+        },
+        currentGoal: {
+          phone: 0,
+          text: 2,
+          beer: 0,
+        },
+        cadence: 'monthly',
+      },
+    },
+    {
+      name: 'Jesus',
+      icon: 'fa-book',
+      friendId: '8',
+      description: 'x gon give it to ya',
+      friendScore: 25,
+      goalSetCollection: {
+        targetGoal: {
+          phone: 1,
+          text: 5,
+          beer: 2,
+        },
+        currentGoal: {
+          phone: 0,
+          text: 2,
+          beer: 0,
+        },
+        cadence: 'monthly',
+      },
+    },
+  ],
+  setting: 'the best setting, foEVA',
+};

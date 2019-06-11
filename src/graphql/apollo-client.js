9ef2a1549a09bf2069aee7cbe2cfcf444ae2605f
dhuddell <dan.huddell@gmail.com>
2019-06-10 20:36:49 -0400

    added auth link to context

--- src/graphql/apollo-client.js
+++ src/graphql/apollo-client.js
@@ -3,8 +3,9 @@ import { ApolloLink } from 'apollo-link';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import httpLink from './http-link';
 import errorLink from './error-link';
+import authLink from './auth-link';
 
-const link = ApolloLink.from([errorLink, httpLink]);
+const link = ApolloLink.from([errorLink, httpLink, authLink]);
 const httpClient = new ApolloClient({
   link,
   cache: new InMemoryCache(),

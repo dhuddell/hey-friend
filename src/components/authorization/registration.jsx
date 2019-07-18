ec71add7fc07a4d3b2122dfed08d237558377f7e
dhuddell <dan.huddell@gmail.com>
2019-07-17 21:57:34 -0400

    more merging

--- src/components/authorization/registration.jsx
+++ src/components/authorization/registration.jsx
@@ -1,31 +1,75 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Mutation } from 'react-apollo';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import { RegisterUser } from '../../graphql/mutations';
 
 const Registration = () => (
-  <div className="registration authorization-form">
-    <div className="form-box">
-      <div className="form-title">
-        <span className="form-title-text">
-          Registration
-        </span>
+  <Mutation mutation={RegisterUser}>
+    {(registerUser, { data }) => (
+      <div className="registration authorization-form">
+        <div className="form-box">
+          <div className="form-title">
+            <span className="form-title-text">
+              Registration
+            </span>
+          </div>
+          <Formik
+            intialValues={{ username: '', password: '' }}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                registerUser({
+                  variables: {
+                    username: this.props.username,
+                    password: this.props.friendId,
+                  },
+                });
+                console.log(values, 'user created.'); // eslint-disable-line
+                setSubmitting(false);
+              }, 500);
+            }}
+            validationSchema={Yup.object().shape({
+              username: Yup.string().required('Required'),
+              password: Yup.string().required('Required'),
+            })}
+          >
+            {(formikProps) => {
+              const {
+                handleSubmit,
+                values,
+                isSubmitting,
+                handleBlur,
+              } = formikProps;
+              return (
+                <form onSubmit={handleSubmit}>
+                  <div className="form-cell">Username</div>
+                  <input
+                    id="username"
+                    className="authorization-field"
+                    placeholder="..clever247"
+                    value={values.username}
+                    onBlur={handleBlur}
+                  />
+                  <div className="form-cell">Password</div>
+                  <input className="authorization-field" placeholder="..h4rdtoh4ck"/>
+                  <button
+                    type="submit"
+                    disabled={isSubmitting}
+                    className="btn modal-btn authorization-btn">
+                      Register
+                  </button>
+                </form>
+              );
+            }}
+          </Formik>
+        </div>
+        <div className="login-link">
+          <Link to="/login">Registered? Click here to login!</Link>
+        </div>
       </div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log('logged in or something');
-        }}
-      >
-        <div className="form-cell">Username</div>
-        <input className="authorization-field" placeholder="..clever247"/>
-        <div className="form-cell">Password</div>
-        <input className="authorization-field" placeholder="..h4rdtoh4ck"/>
-        <button type="submit" className="btn modal-btn authorization-btn">Register</button>
-      </form>
-    </div>
-    <div className="login-link">
-      <Link to="/login">Registered? Click here to login!</Link>
-    </div>
-  </div>
+    )}
+  </Mutation>
 );
 
 export default Registration;
--- src/graphql/mutations/index.js
+++ src/graphql/mutations/index.js
@@ -1,1 +1,2 @@
 export { default as UpdateFriendGoals } from './update-friend-goals.graphql';
+export { default as RegisterUser } from './register-user.graphql';

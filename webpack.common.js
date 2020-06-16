fae02981dadb4eda2de130e6f567be5839de7036
Dan Huddell <dan-huddell@pluralsight.com>
2020-06-16 13:57:43 -0400

    broken attempt at working build+deploy

---
+++ webpack.common.js
@@ -0,0 +1,96 @@
+require('@babel/polyfill');
+require('dotenv').config();
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const Webpack = require('webpack');
+
+module.exports = (env) => {
+  const config = {
+    devServer: {
+      port: 3000,
+      historyApiFallback: true,
+      proxy: {
+        '/graphql': process.env.SERVE_HEY_FRIEND,
+      },
+    },
+    devtool: 'source-map',
+    // entry: ['@babel/polyfill', './src/index.jsx'],
+    entry: [
+      'webpack-dev-server/client?http://localhost:3000/',
+      // {
+      //   app: './src/index.jsx',
+      // },
+    ],
+    output: {
+      filename: 'hey-friend-client.bundle.js',
+      publicPath: '/',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    node: {
+      fs: 'empty',
+    },
+    module: {
+      rules: [
+        { // babel
+          exclude: [
+            /node_modules/,
+            /dist/,
+            /coverage/,
+          ],
+          test: /\.(js|jsx)$/,
+          loader: 'babel-loader',
+        },
+        { // sass
+          test: /\.scss$/,
+          use: [
+            // this might be right???
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+                sassOptions: {
+                  outputStyle: 'compressed',
+                },
+              },
+            },
+          ],
+        },
+        {
+          exclude: [
+            /node_modules/,
+            /dist/,
+            /coverage/,
+          ],
+          test: /\.graphql$/,
+          loader: 'graphql-tag/loader',
+        }, // handle file types
+        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=image/svg+xml' },
+        { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=application/font-woff' },
+        { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=application/font-woff' },
+        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?mimetype=application/octet-stream' },
+        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader' },
+      ],
+    },
+    resolve: {
+      extensions: ['.mjs', '.js', '.jsx'],
+    },
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+    ],
+  };
+
+  if (env && env.GRAPHQL_MOCK) {
+    config.plugins.push(new Webpack.NormalModuleReplacementPlugin(
+      /graphql\/http-link.js/,
+      './schema-link.js'
+    ));
+  }
+
+  return config;
+};
